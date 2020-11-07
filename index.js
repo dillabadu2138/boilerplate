8f946a61fe7718ef11a4e54be2030e4dc9bad610
dillabadu <dillabadu2138@gmail.com>
2020-11-07 22:34:40 +0900

    initial project

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://dillabadu2138:cjsso9484@boilerplate.hr3e2.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port, () =>
+  console.log('Example app is listening on port ${port}!')
+);
